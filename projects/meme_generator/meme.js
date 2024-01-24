48f6e2c0629721150fa3c43acf9148ec63e1122d
jacknorquist <jack.norquist@outlook.com>
2024-01-24 14:40:50 -0600

    Adding html elments and JS dom conrol

---
+++ projects/meme_generator/meme.js
@@ -0,0 +1,10 @@
+let form = document.querySelector("#form");
+let memeGrid = document.getElementById('meme-grid');
+
+form.addEventListener('submit', function ()){
+  e.preventDefault();
+  let meme = document.createElement('div');
+  let imgSrc = document.querySelector("#url").value;
+  let topText = document.querySelector();
+
+}
