c3f399b101fd12cdc9e91dcb1c9b9823f5f73305
jacknorquist <jack.norquist@outlook.com>
2024-01-26 18:32:50 -0600

    Functionality works. Need to clean up js code and add some css styles

--- projects/meme_generator/meme.js
+++ projects/meme_generator/meme.js
@@ -1,10 +1,58 @@
-let form = document.querySelector("#form");
+let form = document.querySelector(".form");
 let memeGrid = document.getElementById('meme-grid');
+let memesCounter = 0;
 
-form.addEventListener('submit', function ()){
+
+
+
+form.addEventListener('submit', function (e) {
   e.preventDefault();
   let meme = document.createElement('div');
-  let imgSrc = document.querySelector("#url").value;
-  let topText = document.querySelector();
+  meme.setAttribute('class', "p-2 meme col-4");
 
-}
+
+
+  let topText = document.createElement('h1');
+  topText.innerHTML = document.querySelector("#text-on-top").value;
+  topText.setAttribute('class', "top-text");
+
+  let bottomText = document.createElement('h1');
+  bottomText.innerHTML = document.querySelector("#text-on-bottom").value;
+  bottomText.setAttribute('class', 'bottom-text');
+
+  let img = document.createElement('img');
+  img.src = document.querySelector("#url").value;
+  img.setAttribute('class', 'meme-img');
+
+
+  let deleteButton = document.createElement('button');
+  deleteButton.setAttribute('class', "dlt-btn");
+  deleteButton.setAttribute('id', memesCounter + 1);
+  deleteButton.innerHTML = "XXXXXX";
+
+
+  meme.appendChild(topText);
+  meme.appendChild(bottomText);
+  meme.appendChild(img);
+  meme.appendChild(deleteButton);
+
+  memeGrid.appendChild(meme);
+
+  document.querySelector("form").reset();
+
+  meme.setAttribute('id', memesCounter + 1);
+  deleteButton.setAttribute('id', memesCounter + 1);
+
+  memesCounter++;
+
+
+  deleteButton.addEventListener('click', function () {
+    let dltID = deleteButton.id;
+    let divDlt = document.getElementById(dltID);
+    console.log(divDlt);
+    divDlt.remove();
+  });
+
+});
+
+
